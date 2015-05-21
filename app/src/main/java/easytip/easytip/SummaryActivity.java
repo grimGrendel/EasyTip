b282d3db19a281b8eec04c10b3f4774ad04ca4f3
pboulet <pboul037@gmail.com>
2015-05-20 20:10:03 -0400

    initial summary activity structure

--- app/src/main/java/easytip/easytip/SummaryActivity.java
+++ app/src/main/java/easytip/easytip/SummaryActivity.java
@@ -4,14 +4,55 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class SummaryActivity extends Activity {
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.summary_expandable_list_view);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+    }
+
+    /*
+     *   Preparing the list data
+     */
+    private void prepareListData(){
+        listDataHeader = new ArrayList<>();
+        listDataChild = new HashMap<>();
+
+        // adding header
+        listDataHeader.add("Bill Summary By Person");
+
+        // adding child data
+        List<String> personBills = new ArrayList<>();
+        personBills.add("Person's 1 Bill");
+        personBills.add("Person's 2 Bill");
+        personBills.add("Person's 3 Bill");
+        personBills.add("Person's 4 Bill");
+
+        listDataChild.put(listDataHeader.get(0), personBills);
     }
 
     @Override
