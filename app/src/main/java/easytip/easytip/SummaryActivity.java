c5c05d7f6eaec6bfea80a4d8109e2b2b2ae7e171
pboulet <pboul037@gmail.com>
2015-05-21 14:59:58 -0400

    imported missing class EditText for WelcomActivity

--- app/src/main/java/easytip/easytip/SummaryActivity.java
+++ app/src/main/java/easytip/easytip/SummaryActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
 import android.widget.RatingBar;
