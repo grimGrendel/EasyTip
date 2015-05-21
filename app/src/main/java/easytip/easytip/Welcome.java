99297fdf0cc48c6cb33e2c0fabf9992e6a52ac4b
grimGrendel <pascaltl@hotmail.com>
2015-05-21 10:45:25 -0400

    Quelques welcome stuff

--- app/src/main/java/easytip/easytip/Welcome.java
+++ app/src/main/java/easytip/easytip/Welcome.java
@@ -1,17 +1,58 @@
 package easytip.easytip;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 
-public class Welcome extends ActionBarActivity {
+public class Welcome extends Activity {
+
+    private EditText billAmountET;
+    private EditText tipPercentET;
+    private RatingBar ratingRB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        // get the bill amount
+        billAmountET = (EditText) findViewById(R.id.welcome_bill_amount_input_field);
+
+        // get the tip percentage
+        // When the tip percentage is set, it reset the star rating to 0
+        tipPercentET = (EditText) findViewById(R.id.welcome_tip_amount_input_field);
+        tipPercentET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!String.valueOf(s).equals(""))
+                    ratingRB.setRating(0f);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        // get the star rating
+        // When the star rating is set, it reset the tip percentage to 0
+        ratingRB = (RatingBar) findViewById(R.id.welcome_rating_bar);
+        ratingRB.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                tipPercentET.setText("", TextView.BufferType.EDITABLE);
+            }
+        });
     }
 
     @Override
@@ -34,5 +75,18 @@ public class Welcome extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void welcomeSubmitButtonClicked(View view) {
+
+        // Changes layout from Welcome to Summary
+        Intent getSummaryScreenIntent = new Intent (this, SummaryActivity.class);
+
+        final int result = 1;
+
+        getSummaryScreenIntent.putExtra("callingSummaryActivity", "Welcome");
+
+        startActivity(getSummaryScreenIntent);
+
     }
 }
